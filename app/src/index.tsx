3a3e8b2181a0ee4ba60b8f471231c78dcfd4b12a
denhampreen <denhampreen@gmail.com>
2019-08-03 13:17:05 +0200

    Updates

--- app/src/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AppRoutes from './App';
-
-ReactDOM.render(<AppRoutes />, document.getElementById('root'));
---
+++ precache-manifest.b96bbfa8098dfb863afbd2a0751cb884.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8cf642ac73fda3586ad2ec3c9884ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82a51e2c6e11e56f3883",
+    "url": "/static/css/main.6a3e33db.chunk.css"
+  },
+  {
+    "revision": "ccc1947fd362c02b88bb",
+    "url": "/static/js/2.24bdcb18.chunk.js"
+  },
+  {
+    "revision": "82a51e2c6e11e56f3883",
+    "url": "/static/js/main.37ad0ec9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "195648cb6f212610c8a65288aab0247c",
+    "url": "/static/media/wildcards.195648cb.png"
+  }
+]);
