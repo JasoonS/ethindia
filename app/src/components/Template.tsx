912175808a4eb8cefa1a7b602ea9bccbd84f3fbe
jonjon <jonjonclark@gmail.com>
2019-08-03 18:03:43 +0530

    added how it works section

--- app/src/components/Template.tsx
+++ app/src/components/Template.tsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import Toggle from 'react-toggle'
-import Dapp from "./Dapp"
-import About from './About'
+import React, { Component } from 'react';
+import Toggle from 'react-toggle';
+import Dapp from './Dapp';
+import About from './About';
+import HowItWorks from './HowItworks';
 
 type displayPurchaseState = {
-  displayPurchase: boolean
-}
+  displayPurchase: boolean;
+};
 
 class Template extends Component<{}, displayPurchaseState> {
-
   componentWillMount() {
     this.setState({
       displayPurchase: false
@@ -16,14 +16,15 @@ class Template extends Component<{}, displayPurchaseState> {
   }
 
   render() {
-
     return (
       <div>
         <header>
           <h1>
-            These ads are always for sale, purchase the ad space and set your own monthly payment...
-            <br /> and even make a profit when someone buys the ad space from you
-            </h1>
+            These ads are always for sale, purchase the ad space and set your
+            own monthly payment...
+            <br /> and even make a profit when someone buys the ad space from
+            you
+          </h1>
           {/* <label>
             <Toggle
               defaultChecked={this.state.displayPurchase}
@@ -34,46 +35,104 @@ class Template extends Component<{}, displayPurchaseState> {
             <span>Purchase Ad Space</span>
           </label> */}
         </header>
-        <div className="Grid">
-          <div className="Grid-item">
+        <div className='Grid'>
+          <div className='Grid-item'>
             <Dapp displayPurchase={this.state.displayPurchase} />
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
-          <div className="Grid-item">
-            <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
+          <div className='Grid-item'>
+            <a href='#'>
+              <img
+                src='https://via.placeholder.com/300'
+                style={{ width: '100%' }}
+              />
+            </a>
           </div>
         </div>
         <footer>
           <div className='socials'>
-            <div className="fb-share-button" data-href="http://alwaysforsale.io" data-layout="button" data-size="small">
-              <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Falwaysforsale.io%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a>
+            <div
+              className='fb-share-button'
+              data-href='http://alwaysforsale.io'
+              data-layout='button'
+              data-size='small'
+            >
+              <a
+                target='_blank'
+                href='https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Falwaysforsale.io%2F&amp;src=sdkpreparse'
+                className='fb-xfbml-parse-ignore'
+              >
+                Share
+              </a>
             </div>
             <div>
-              <a href="https://twitter.com/intent/tweet?button_hashtag=Alwaysforsale.io&ref_src=twsrc%5Etfw" className="twitter-hashtag-button" data-show-count="false">Tweet #Alwaysforsale</a>
+              <a
+                href='https://twitter.com/intent/tweet?button_hashtag=Alwaysforsale.io&ref_src=twsrc%5Etfw'
+                className='twitter-hashtag-button'
+                data-show-count='false'
+              >
+                Tweet #Alwaysforsale
+              </a>
             </div>
           </div>
           <div className='more-info'>
             <About />
+            <HowItWorks />
           </div>
         </footer>
       </div>
@@ -81,4 +140,4 @@ class Template extends Component<{}, displayPurchaseState> {
   }
 }
 
-export default Template
+export default Template;
