478f3b91a1f3116546749a48ed3ccd313022c649
denhampreen <denhampreen@gmail.com>
2019-08-03 12:47:50 +0200

    social share buttons

--- app/src/components/Template.tsx
+++ app/src/components/Template.tsx
@@ -20,7 +20,8 @@ class Template extends Component<{}, displayPurchaseState> {
       <div>
         <header>
           <h1>
-            These ads are always for sale, purchase the ad space and set your own monthly payment
+            These ads are always for sale, purchase the ad space and set your own monthly payment...
+            <br /> and even make a profit when someone buys the ad space from you
             </h1>
           {/* <label>
             <Toggle
@@ -61,6 +62,14 @@ class Template extends Component<{}, displayPurchaseState> {
             <a href="#"><img src="https://via.placeholder.com/300" style={{ width: '100%' }} /></a>
           </div>
         </div>
+        <footer>
+          <div className="fb-share-button" data-href="http://alwaysforsale.io" data-layout="button" data-size="small">
+            <a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Falwaysforsale.io%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore">Share</a>
+          </div>
+          <div>            
+            <a href="https://twitter.com/intent/tweet?button_hashtag=Alwaysforsale.io&ref_src=twsrc%5Etfw" className="twitter-hashtag-button" data-show-count="false">Tweet #Alwaysforsale</a>
+          </div>
+        </footer>
       </div>
     );
   }
