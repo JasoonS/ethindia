6d4ee24c687b8e79cac48b7e1df751e769429820
jonjon <jonjonclark@gmail.com>
2019-08-03 12:42:35 +0530

    scalable harberger contracts added

---
+++ app/src/components/About.js
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About our always for sale adspace</h1>
+      <p>
+        This project was created at the #ETHIndia2019 hackathon in roughly
+        35hrs. The purpose of the project is two fold:
+      </p>
+      <p>
+        {' '}
+        1. To generate funds for wildlife conservation. How? We created a
+        project called wildcards.world at the #ETHCapeTown2019 hackathon to
+        raise funds for wildlife conservation. Subsequently, we have got into a
+        6 month blockchain incubator in Paris to accelerate the developement of
+        this platform. The funds generated from our #ETHIndia 'always for sale
+        adspace' will provide funding for us to devlop our wildcards project and
+        hence raise funds for conservation efforts.{' '}
+      </p>
+      <p>
+        2. This project is a continued social experiment to understand how
+        always for sale adspace might behave in a viral marketplace. Many of you
+        may be famiailir of the milliondollarhomepage and how this simple
+        project sold a million pixels at a dolar each. This extends this concept
+        but rather aims at scarcity and introduces a very interesting concept
+        where each piece of marketing real estate will be always for sale,
+        forever.
+      </p>
+      <p>
+        The success of this project will allow us to provide always for sale
+        plugin marketing solutions to other companies and continue to fund the
+        developement of wildcards - the exploration of sustainable solutions
+        using an always for sale mechanic.
+      </p>
+      <p>
+        Join our telegram: https://t.me/wildcardsworld Email us to chat:
+        jonjon@wildcards.world
+      </p>
+      <p>Made by Denham Preen and JonJon Clark</p>
+      <p>Version: 1.0.1</p>
+    </Fragment>
+  );
+};
+
+export default About;
