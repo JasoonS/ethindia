0515e82a3972403ceb4d1dede3e39fcef66d19ec
denhampreen <denhampreen@gmail.com>
2019-08-04 04:19:07 +0200

    Updates

---
+++ precache-manifest.9d551d636c31775c577dc2b778b568a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98151f53c852a5fa90b03f79f7a8c383",
+    "url": "./index.html"
+  },
+  {
+    "revision": "aa74fe4114a5f9f40ab8",
+    "url": "./static/css/2.b1119605.chunk.css"
+  },
+  {
+    "revision": "a11c0abc82b4c56a16cc",
+    "url": "./static/css/main.002cbf2f.chunk.css"
+  },
+  {
+    "revision": "aa74fe4114a5f9f40ab8",
+    "url": "./static/js/2.ed3df3f3.chunk.js"
+  },
+  {
+    "revision": "a11c0abc82b4c56a16cc",
+    "url": "./static/js/main.184deafc.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
